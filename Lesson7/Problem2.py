69a240b86f0a38ff9fd8b904f1b99d06ad0ddb39
ariiellaa <44405336+ariiellaa@users.noreply.github.com>
2018-12-12 12:41:30 -0500

    Add files via upload

---
+++ Lesson7/Problem2.py
@@ -0,0 +1,8 @@
+x = input('pick a number:')
+x = int(x)
+y = input('pick another number:')
+y = int(y)
+
+total = x * y
+total = str(total)
+print(total)
---
+++ Lesson7/Problem3.py
@@ -0,0 +1,7 @@
+print( '_' * 60 )
+age = input('What is your age? ')
+age = int(age)
+afteryears = 100 - age 
+answer = 2018 + afteryears
+print(answer)
+print( '_' * 60 )
